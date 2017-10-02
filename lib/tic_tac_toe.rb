4e5c6dd3406590465509e6a5e45fd619ac8abb34
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-10-02 15:46:37 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -76,9 +76,6 @@ def won?
            if winning_combination.all? {|i| @board[i] == "X"} || winning_combination.all?{|i| @board[i] == "O"}
              result = winning_combination
            end
-          else
-            false
-           end
      end
 
 def full?(board)
