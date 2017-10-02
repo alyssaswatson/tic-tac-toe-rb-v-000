47d2b500b243ea842e13d57c4d600ac1acc2b12b
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-10-02 15:40:10 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -71,24 +71,15 @@ WIN_COMBINATIONS = [
   [0,4,8],
 ]
 
-def won?(board)
-
-   WIN_COMBINATIONS.detect do |win_combination|
-     win_index_1 = win_combination[0]
-     win_index_2 = win_combination[1]
-     win_index_3 = win_combination[2]
-     position_1 = board[win_index_1]
-     position_2 = board[win_index_2]
-     position_3 = board[win_index_3]
-     if position_1 == "X" && position_2 == "X" && position_3 == "X"
-       return win_combination
-     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-       return win_combination
-     else
-       false
+def won?
+  result = false
+       WIN_COMBINATIONS.each do |winning_combination|
+           if winning_combination.all? {|i| @board[i] == "X"} || winning_combination.all?{|i| @board[i] == "O"}
+             result = winning_combination
+           end
+         end
+       return result
      end
-   end
-end
 
 def full?(board)
    if board.any?{|space| space==" "}
